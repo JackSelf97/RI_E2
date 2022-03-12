4648e384c4e1afe3486683d6b459e4ed324ef7fe
JackSelf97 <selfbrothers@hotmail.com>
2022-03-12 17:14:22 +0000

    Trash_Script+ & Window_Script+
    - Scripts added

---
+++ RI_E2 - Falmouth University/Assets/Scripts/Trash_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash_Script : MonoBehaviour
+{
+    private Rigidbody myRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody>();
+        myRb.AddForce(Vector3.up * 8, ForceMode.Impulse);
+        myRb.AddForce(-Vector3.forward * 3, ForceMode.Impulse);
+
+        var ranNo = Random.Range(-1f, 2f);
+        myRb.AddForce(new Vector3(ranNo, 0, 0) * 2, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RI_E2 - Falmouth University/Assets/Scripts/Window_Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window_Script : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject[] trash;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+
+            var ranNo = Random.Range(0, 3);
+            Debug.Log(ranNo);
+            Instantiate(trash[ranNo], spawnPoint.transform.position, Quaternion.identity);
+            Debug.Log("Here comes some trash!");
+            timer = 0; // resets
+
+
+
+
+
+        }
+
+        
+    }
+}
