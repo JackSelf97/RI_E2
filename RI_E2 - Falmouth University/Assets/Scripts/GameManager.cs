ff2a4decf1661b73306443f16716b367eef8983c
JackSelf97 <selfbrothers@hotmail.com>
2022-03-15 15:41:10 +0000

    Level Design (0.1)

---
+++ RI_E2 - Falmouth University/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float time = 120f;
+    public Text countdownTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        //time = Mathf.Round(time * 100f) / 100f;
+        countdownTxt.text = "Time: " + time.ToString("00");
+    }
+}
